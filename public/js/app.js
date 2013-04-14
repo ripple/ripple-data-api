fe90ec93a211b547feb61dd406b34a6379216817
Stefan Thomas <justmoon@members.fsf.org>
2013-04-14 18:29:47 +0200

    Dashboard should have root URL.

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard', controller: DashboardCtrl});
+    $routeProvider.when('/', {templateUrl: 'partials/dashboard', controller: DashboardCtrl});
     $routeProvider.when('/network', {templateUrl: 'partials/network'});
     $routeProvider.when('/market/:first/:second', {templateUrl: 'partials/market',
                                                    controller: MarketCtrl});
-    $routeProvider.otherwise({redirectTo: '/dashboard'});
+    $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);
 
 // UserVoice
-(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/BZAhKCSSiDVOI9b4eNA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/BZAhKCSSiDVOI9b4eNA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s);})();
