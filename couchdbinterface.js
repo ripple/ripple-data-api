9fa8048b8c5ca09f990eeafc7ba81233e1f3edfd
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-11-03 22:46:37 -0800

    working on functions to write trades, xrp balances, and gateways to csv

--- couchdbinterface.js
+++ couchdbinterface.js
@@ -7,6 +7,12 @@ var winston = require('winston'),
 var config = require('./config');
 var db = require('nano')('http://' + config.couchdb.username + ':' + config.couchdb.password + '@' + config.couchdb.host + ':' + config.couchdb.port + '/' + config.couchdb.database);
 
+getTrades(["USD", "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"],
+          ["XRP"],
+          "2013-09-01",
+          "2013-11-03",
+          "day");
+// getGateways();
 
 
 function getXrpBalances() {
@@ -27,7 +33,7 @@ function getXrpBalances() {
             if (typeof acct.Account === "string" && typeof acct.Balance === "string")
                 genesis_accounts[acct.Account] = {
                     in_later_ledgers: false,
-                    balance: parseInt(acct.Balance)
+                    balance: parseInt(acct.Balance, 10)
                 };
         });
 
@@ -91,7 +97,7 @@ function getXrpBalances() {
             _.each(Object.keys(genesis_accounts), function(acct_addr) {
                 if (genesis_accounts[acct_addr].in_later_ledgers === false) {
                     var bal = genesis_accounts[acct_addr].balance;
-                    fs.appendFile(filename, ("" + acct_addr + "," + bal + "\n"));
+                    fs.appendFileSync(filename, ("" + acct_addr + "," + bal + "\n"));
                 }
             });
 
@@ -104,24 +110,29 @@ function getXrpBalances() {
 }
 
 
-getTrades(["USD", "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"], ["XRP"], "2013-10-1", "2013-11-1", "month");
 
 function getTrades(curr1, curr2, start, end, time_period) {
     // curr1 and curr2 should be arrays of the form ["CUR", "r...issuer"]
 
     var filename = "./spreadsheets/trades_" + 
-                    curr1[0] + (curr1.length === 2 ? curr1[1] : "") + 
-                    "_for_" + 
-                    curr2[0] + (curr2.length === 2 ? curr2[1] : "") + "_" + 
-                    start + "_to_" + end + 
-                    "_by_" + time_period + 
+                    curr1[0] + (curr1.length === 2 ? curr1[1] : "") +
+                    "_for_" +
+                    curr2[0] + (curr2.length === 2 ? curr2[1] : "") + "_" +
+                    start + "_to_" + end +
+                    "_by_" + time_period +
                     ".csv";
 
     var time_periods = ["year", "month", "day", "hour", "minute", "second"],
         period_index = (time_periods.indexOf(time_period) >= 0 ? time_periods.indexOf(time_period) : 3),
-        start_array = moment(start).utc().toArray().slice(0, period_index + 1),
-        end_array = moment(end).utc().toArray().slice(0, period_index + 1),
+        start_array = moment(start + " 00:00:00 +0000").utc().toArray(),
+        end_array = moment(end + " 00:00:00 +0000").utc().toArray(),
         group_level = period_index + 3;
+
+    // winston.info("startkey:", JSON.stringify(start_array));
+    // winston.info("endkey:", JSON.stringify(end_array));
+
+    start_array = start_array.slice(0, period_index + 1).concat([0, 0, 0, 0, 0, 0].slice(period_index + 1));
+    end_array = end_array.slice(0, period_index + 1).concat([9999, 12, 31, 23, 59, 59].slice(period_index + 1));
 
     var startkey = [curr1, curr2].concat(end_array),
         endkey = [curr1, curr2].concat(start_array);
@@ -133,23 +144,32 @@ function getTrades(curr1, curr2, start, end, time_period) {
         group_level: group_level
     };
 
-    var csv_header = ["timestamp", "open", "high", "low", "close", "vwav", "curr1_volume", "curr2_volume"].join(',') + "\n";
+    winston.info("startkey:", JSON.stringify(startkey));
+    winston.info("endkey:", JSON.stringify(endkey));
+    winston.info("group_level:", group_level);
+
+
+    var csv_header = ["timestamp","open_time", "close_time", "open", "high", "low", "close", "vwav", "curr1_volume", "curr2_volume", "num_trades"].join(',') + "\n";
     fs.writeFileSync(filename, csv_header);
 
     function writeRow (filename, row) {
+        // console.log(JSON.stringify(row));
         var key = row[0],
             value = row[1];
             var time = moment(key.slice(2)).utc().format();
 
             var csv_row = [
                 time,
+                moment(value.open_time).utc().format(),
+                moment(value.close_time).utc().format(),
                 value.open,
                 value.high,
                 value.low,
                 value.close,
                 value.volume_weighted_avg,
                 value.curr1_volume,
-                value.curr2_volume
+                value.curr2_volume,
+                value.num_trades
             ].join(',') + "\n";
 
             fs.appendFile(filename, csv_row);
@@ -169,7 +189,7 @@ function getTrades(curr1, curr2, start, end, time_period) {
 
     req.on("error", function(err) {
         if (err) {
-            winston.error("Error getting xrp_totals:", err);
+            winston.error("Error getting offersExercised:", err);
             return;
         }
     });
@@ -195,39 +215,51 @@ function getTrades(curr1, curr2, start, end, time_period) {
 }
 
 
-// function getGateways (startkey) {
 
-//     db.view("rphistory", "trustlinesByCurrency", {group_level: 2, limit: 1000}, function(err, body){
-//         if (err) {
-//             winston.info("Error getting trustlinesByCurrency:", err);
-//             return;
-//         }
+function getGateways (startkey) {
 
-//         var gateways = {};
-//         _.each(body.rows, function(row){
-//             var acct = row.key[1],
-//                 curr = row.key[0];
+    var filename = "./spreadsheets/gateways_" + moment().format("YYYY-MM-DD");
 
-//             /* 
-//             Gateway heuristic:
-//                 > 100 incoming trustlines
-//                 < 0 balance for given currency
-//             */
-//             if (row.value.incoming >= 100
-//                 && row.value.balance_change < 0) {
+    var csv_header = ["account", "currency", "trust_in", "trust_out", "balance_change"].join(",") + "\n";
+    fs.writeFileSync(filename + ".csv", csv_header);
 
-//                 if (typeof gateways[acct] === "undefined")
-//                     gateways[acct] = {};
+    db.view("trustlines", "trustlineStatsCurrency", {group_level: 2}, function(err, body){
+        if (err) {
+            winston.info("Error getting trustlineStatsCurrency:", err);
+            return;
+        }
 
-//                 gateways[acct][curr] = {
-//                     trust_in: row.value.incoming,
-//                     trust_out: row.value.outgoing,
-//                     balance: row.value.balance_change
-//                 };
-//             }
-//         });
+        var gateways = {};
+        _.each(body.rows, function(row){
+            var acct = row.key[1],
+                curr = row.key[0];
 
-//         winston.info(JSON.stringify(gateways));
-//         getGateways(body.rows[body.rows.length - 1]);
-//     });
-// }
+            /* 
+            Gateway heuristic:
+                > 50 incoming trustlines
+                < 0 balance for given currency
+            */
+            if (row.value.incoming >= 50
+                && row.value.balance_change < 0) {
+
+                if (typeof gateways[acct] === "undefined")
+                    gateways[acct] = {};
+
+                gateways[acct][curr] = {
+                    trust_in: row.value.incoming,
+                    trust_out: row.value.outgoing,
+                    balance: row.value.balance_change
+                };
+            
+                var csv_row = [acct, curr, row.value.incoming, row.value.outgoing, row.value.balance_change].join(",") + "\n";
+                fs.appendFileSync(filename + ".csv", csv_row);
+            }
+
+        });
+
+        fs.writeFileSync(filename + ".json", JSON.stringify(gateways));
+        // winston.info(JSON.stringify(gateways));
+    });
+}
+
+
