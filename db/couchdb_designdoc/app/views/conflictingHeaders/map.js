199fde8c03e65ede0959014cb6388e97f8f71bdc
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-11-20 16:27:55 -0800

    added new payment view

--- db/couchdb_designdoc/app/views/conflictingHeaders/map.js
+++
@@ -1,5 +0,0 @@
-function (doc) {
-  if (doc.conflicting_ledger_headers && doc.conflicting_ledger_headers.length > 0) {
-    emit(doc.ledger_index, doc);
-  }
-}
--- db/couchdb_designdoc/app/views/transactionsByAccount/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-    // doc will be a JSON object of the same form as: http://ct.ripple.com:5984/_utils/document.html?rphist/0003100000
-
-    var key = [];
-    var value = {};
-
-    // emit is CouchDB's function that is effectively "return" for map functions
-    emit(key, value);
-}
