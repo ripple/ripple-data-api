4e2231387db441d9dd31de905cf0561f9c47ede9
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-12-03 17:00:53 -0500

    added accountsCreated api call

--- api/app.js
+++ api/app.js
@@ -23,6 +23,7 @@ var DATEARRAY = ['YYYY', '-MM', '-DD', 'THH', ':mm', ':ssZZ'],
 app.use(express.bodyParser());
 
 
+// TODO create one app.post('/api/*') handler that processes requests
 
 /**
  *  offersExercised returns reduced or individual 
@@ -243,9 +244,160 @@ app.post('/api/offersExercised/', function (req, res) {
 
   });
 
+});
+
+
+/**
+ *  accountsCreated returns the number of accounts created per time increment
+ *  expects req.body to have:
+ *  {
+ *    timeIncrement: (any of the following: "all", "year", "month", "day", "hour", "minute", "second") // optional, defaults to "day"
+ *    startTime: (any momentjs-readable date), // optional, defaults to now if descending is true, 30 days ago otherwise
+ *    endTime: (any momentjs-readable date), // optional, defaults to 30 days ago if descending is true, now otherwise
+ *    staleView: true/false, // optional, defaults to true
+ *    descending: true/false, // optional, defaults to true
+ *  }
+ */
+app.post('/api/accountsCreated/', function(req, res){
+
+  var viewOpts = {};
+
+  // parse startTime and endTime
+  // TODO handle incorrect startTime/endTime values
+  var startTime, endTime;
+  if (!req.body.startTime && !req.body.endTime) {
+    // default
+    startTime = moment.utc().subtract('days', 30);
+    endTime = moment.utc();
+  } else if (moment(req.body.startTime).isBefore(moment(req.body.endTime))) {
+    startTime = moment.utc(req.body.startTime);
+    endTime = moment.utc(req.body.endTime);
+  } else {
+    endTime = moment.utc(req.body.startTime);
+    startTime = moment.utc(req.body.endTime);
+  } 
+
+  // handle descending/non-descending query
+  if (!req.body.hasOwnProperty('descending') || req.body.descending === true) {
+    viewOpts.descending = true;
+
+    // swap startTime and endTime if results will be in descending order
+    var tempTime = startTime;
+    startTime = endTime;
+    endTime = tempTime;
+  } else {
+    // TODO handle incorrect values
+    winton.error('Inccorect descending value. descending: ' + req.body.descending);
+  }
+
+  // set startkey and endkey for couchdb query
+  viewOpts.startkey = startTime.toArray().slice(0,6);
+  viewOpts.endkey = endTime.toArray().slice(0,6);
+
+  // always reduce
+  viewOpts.reduce = true;
+
+  // determine the group_level from the timeIncrement field
+  if (req.body.timeIncrement) {
+    var inc = req.body.timeIncrement.toLowerCase(),
+      levels = ['year', 'month', 'day', 'hour', 'minute', 'second'];
+    if (inc === 'all') {
+      viewOpts.group = false;
+    } else if (levels.indexOf(inc)) {
+      viewOpts.group_level = 1 + levels.indexOf(inc);
+    } else {
+      viewOpts.group_level = 1 + 2; // default to day
+    }
+  } else {
+    // TODO handle incorrect options better
+    viewOpts.group_level = 1 + 2; // default to day
+  }
+
+
+  // set stale view option
+  if ((!req.body.hasOwnProperty('stale') && !req.body.hasOwnProperty('staleView'))
+    || req.body.stale || req.body.staleView) {
+    viewOpts.stale = 'update_after';
+  }
+
+  winston.info('viewOpts: ' + JSON.stringify(viewOpts));
+
+  db.view('accounts', 'accountsCreated', viewOpts, function(err, couchRes){
+
+    if (err) {
+      winston.error('Error with request: ' + err);
+      return;
+      // TODO send error messages to api querier
+    }
+
+    winston.info('Got ' + couchRes.rows.length + ' rows');
+    winston.info(JSON.stringify(couchRes.rows));
+
+
+    // prepare results to send back
+    var resRows = [],
+      headerRow = [
+        'time', 
+        'accountsCreated'
+      ];
+
+    resRows.push(headerRow);
+
+    couchRes.rows.forEach(function(row){
+      resRows.push([
+        (row.key ? moment.utc(row.key).format(DATEFORMAT) : ''),
+        row.value
+        ]);
+    });
+
+    // handle format option
+    if (!req.body.format || req.body.format === 'json') {
+
+      // TODO include time sent?
+      res.send(resRows);
+
+    } else if (req.body.format === 'csv') {
+
+      var csvStr = _.map(resRows, function(row){
+        return row.join(', ');
+      }).join('\n');
+
+      // TODO make this download instead of display
+      res.setHeader('Content-disposition', 'attachment; filename=offersExercised.csv');
+      res.setHeader('Content-type', 'text/csv');
+      res.charset = 'UTF-8';
+      res.end(csvStr);
+
+    } else if (req.body.format === 'json_verbose') {
+
+      // send as an array of json objects
+      var apiRes = {};
+      apiRes.timeRetrieved = moment.utc().valueOf();
+
+      apiRes.rows = _.map(couchRes.rows, function(row){
+
+        // reformat rows
+        return {
+          openTime: (row.key ? moment.utc(row.key.slice(2)).format(DATEFORMAT) : moment.utc(row.value.openTime).format(DATEFORMAT)),
+          accountsCreated: row.value
+        };
+
+      });
+
+      res.json(apiRes);
+
+    } else {
+      // TODO handle incorrect input
+      winston.error('incorrect format: ' + req.body.format);
+
+    }
+
+  });
 
 });
 
+
+/** HELPER FUNCTIONS **/
 
 /**
  *  gatewayNameToAddress translates a given name and, 
