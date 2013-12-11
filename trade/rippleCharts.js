06e7aaa86462c28af3577111b0cbdc0cfd150a25
Matthew Fettig <matthew@ripple.com>
2013-12-11 04:20:00 -0800

    candlestick style

--- trade/rippleCharts.js
+++ trade/rippleCharts.js
@@ -198,23 +198,20 @@ RippleCharts.line = function (options){
 };
 
 RippleCharts.candlestick = function (options) {
-
-  var self = this,
-      candleWidth = 10,
-      xScale      = d3.time.scale.utc(),
-      yScale      = d3.scale.linear(),
-      volumeScale = d3.scale.linear(),
-      xAxis       = d3.svg.axis().scale(xScale),
-      leftAxis    = d3.svg.axis().scale(volumeScale).orient("left").tickFormat(d3.format("s")),
-      yAxis       = d3.svg.axis().scale(yScale).orient("right"),
-      format      = String;
+	var self = this,
+    	candleWidth = 10,
+      	xScale      = d3.time.scale.utc(),
+      	yScale      = d3.scale.linear(),
+      	volumeScale = d3.scale.linear(),
+      	xAxis       = d3.svg.axis().scale(xScale),
+      	leftAxis    = d3.svg.axis().scale(volumeScale).orient("left").tickFormat(d3.format("s")),
+      	yAxis       = d3.svg.axis().scale(yScale).orient("right");
 
 	if (!options.margin) options.margin = {top: 10, right: 50, bottom: 50, left: 50};
 	if (!options.height) options.height = 500;
 	if (!options.width)  options.width  = 950;
-	
-	var div = d3.select(options.id);
 
+	var div = d3.select(options.id);
 	var svg = div.selectAll("svg").data([0])
 	var svgEnter = svg.enter().append("svg")
 		.attr("width", options.width + options.margin.left + options.margin.right)
@@ -230,24 +227,31 @@ RippleCharts.candlestick = function (options) {
 		.style("opacity", 0);	
 
 
-      svg.append("defs").append("clipPath").attr("id", "clip").append("rect");
-      
-      var gEnter = svg.append("g");
-      gEnter.append("rect").attr("class", "background");
-      gEnter.append("g").attr("class", "volume");
-      gEnter.append("g").attr("class", "candlesticks").attr("clip-path", "url(#clip)");
-      gEnter.append("g").attr("class", "x axis");
-      gEnter.append("g").attr("class", "left axis").append("text").attr("class", "title").attr("transform", "rotate(-90)").attr("dy", "10px").attr("y", 6).text("Volume");
-      gEnter.append("g").attr("class", "y axis").append("text").attr("class", "title").attr("transform", "rotate(-90)").attr("dy", "10px").attr("y", 950).text("Price");
-      gEnter.append("line").attr("class", "hover");
-      gEnter.append("text").attr("class", "status").attr("y", -9);
-      
+  	svg.append("defs").append("clipPath").attr("id", "clip").append("rect");
+    svg.select("rect").attr("width", options.width).attr("height", options.height);
+	      
+  	var gEnter = svg.append("g")
+  		.attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
+  	gEnter.append("rect").attr("class", "background").attr("width", options.width).attr("height", options.height);
+  	gEnter.append("g").attr("class", "volume");
+  	gEnter.append("g").attr("class", "candlesticks").attr("clip-path", "url(#clip)");
+  	gEnter.append("g").attr("class", "x axis");
+  	gEnter.append("g").attr("class", "left axis").append("text").attr("class", "title").attr("transform", "rotate(-90)").attr("dy", "10px").attr("y", 6).text("Volume");
+  	gEnter.append("g").attr("class", "y axis").append("text").attr("class", "title").attr("transform", "rotate(-90)").attr("dy", "10px").attr("y", 950).text("Price");
+    gEnter.append("circle").attr("class", "focus").attr("r",3).attr("fill", "#555");
+    
+    var hover      = gEnter.append("line").attr("class", "hover").attr("y2", options.height);
+  	var horizontal = gEnter.append("line").attr("class", "hover");
+  	      
+	
+	          
 	this.fadeOut = function () {
 		div.selectAll("svg").transition().duration(100).style("opacity", 0);
-		//svg.on("mousemove.hover", "");
+		svg.on("mousemove.candlestick", "");
 		details.style("opacity", 0);
 		//status.style("opacity", 0);
-		div.selectAll(".hover").style("opacity", 0);	
+		div.selectAll(".hover").style("opacity", 0);
+		div.selectAll(".focus").style("opacity", 0);	
 		loader.style("opacity",1);	
 	}
 		
@@ -257,8 +261,8 @@ RippleCharts.candlestick = function (options) {
 		self.base  = base;
 		self.trade = trade;
 		
-		var end     = new Date;
-	 	var start   = d.offset(end);
+		var end    = new Date;
+	 	var start  = d.offset(end);
 	 	
 	 	if (self.request) self.request.abort();
 	 	self.request = d3.xhr(options.url);
@@ -297,115 +301,122 @@ RippleCharts.candlestick = function (options) {
 	}
 	
 	this.redraw = function (increment, lineData) {
-		//div.datum(lineData);
-		candleWidth = options.width/lineData.length;
-		if (candleWidth<5) candleWidth = 5; 
+
+		candleWidth = options.width/(lineData.length*1.15);
+		if (candleWidth<4) candleWidth = 4; 
 	
-	      // Update the outer dimensions.
-	      svg.datum(lineData)
-	      	.on("mousemove.candlestick", mousemove);
+	    svg.datum(lineData).on("mousemove.candlestick", mousemove);
 	
-	      // Update the inner dimensions.
-	      var g = svg.select("g")
-	          .attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
+	    var g      = svg.select("g");
+		var status = div.select(".status");
+		var focus  = g.select(".focus");
+		
+		var line = g.select(".volume").selectAll(".line").data(function(d) { return [d]; });
+	    line.enter().append("path").attr("class", "line");
 	
-	      svg.select("rect")
-	          .attr("width", options.width)
-	          .attr("height", options.height);
+	    // Update the candlesticks.
+	    var candle = g.select(".candlesticks").selectAll("g").data(function(d) { return d; }, function(d) { return d.time; });
+	    var candleEnter = candle.enter().append("g")
+	        .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; });
+	    candleEnter.append("line")
+	        .attr("y1", function(d) { return yScale(.5 * (d.low + d.high)); })
+	        .attr("y2", function(d) { return yScale(.5 * (d.low + d.high)); });
+	    candleEnter.append("rect")
+	        .attr("x", -candleWidth / 2)
+	        .attr("y", function(d) { return yScale(.5 * (d.open + d.close)); })
+	        .attr("height", function(d) { return Math.abs(yScale(d.open) - yScale(d.close))+.5; })
+	        .attr("width", candleWidth);
+	    candleEnter.append("line")
+	    	.attr("class", "high")
+	    	.attr("x1", -candleWidth / 4)
+	    	.attr("x2", candleWidth / 4);
+	    candleEnter.append("line")
+	    	.attr("class", "low")
+	    	.attr("x1", -candleWidth / 4)
+	    	.attr("x2", candleWidth / 4);	
 	
-	      g.select(".background")
-	          .attr("width", options.width)
-	          .attr("height", options.height);
+	    // Update the x-scale.
+	    xScale
+	        .domain(d3.extent(lineData, function(d) { return d.time; }))
+	        .range([0, options.width]);
 	
-	      var status = div.select(".status");
+	    // Update the volume scale.
+	    volumeScale
+	        .domain(d3.extent(lineData, function(d) { return d.volume; }))
+	        .range([options.height, 0]);
 	
-	      var hover = g.select(".hover")
-	          .attr("y2", options.height);
-	
-	      var line = g.select(".volume").selectAll(".line").data(function(d) { return [d]; });
-	      line.enter().append("path").attr("class", "line");
-	
-	      // Update the candlesticks.
-	      var candle = g.select(".candlesticks").selectAll("g").data(function(d) { return d; }, function(d) { return d.time; });
-	      var candleEnter = candle.enter().append("g")
-	          .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; });
-	      candleEnter.append("line")
-	          .attr("y1", function(d) { return yScale(.5 * (d.low + d.high)); })
-	          .attr("y2", function(d) { return yScale(.5 * (d.low + d.high)); })
-	      candleEnter.append("rect")
-	          .attr("x", -candleWidth / 2)
-	          .attr("y", function(d) { return yScale(.5 * (d.open + d.close)); })
-	          .attr("height", function(d) { return Math.abs(yScale(d.open) - yScale(d.close)); })
-	          .attr("width", candleWidth);
-	
-	      // Update the x-scale.
-	      xScale
-	          .domain(d3.extent(lineData, function(d) { return d.time; }))
-	          .range([0, options.width]);
-	
-	      // Update the volume scale.
-	      volumeScale
-	          .domain(d3.extent(lineData, function(d) { return d.volume; }))
-	          .range([options.height, 0]);
-	
-	      // Update the y-scale.
-	      yScale
-	          .domain([
-	            d3.min(lineData, function(d) { return Math.min(d.open, d.close, d.high, d.low); }),
+	    // Update the y-scale.
+	    yScale
+	    	.domain([
+	        	d3.min(lineData, function(d) { return Math.min(d.open, d.close, d.high, d.low); }),
 	            d3.max(lineData, function(d) { return Math.max(d.open, d.close, d.high, d.low); })
-	          ])
-	          .range([options.height, 0]);
+	        ])
+	  		.range([options.height, 0]);
 	
-	      var candleUpdate = d3.transition(candle.classed("up", function(d) { return d.open <= d.close; }))
-	          .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; });
-	      candleUpdate.select("line")
-	          .attr("y1", function(d) { return yScale(d.low); })
-	          .attr("y2", function(d) { return yScale(d.high); });
-	      candleUpdate.select("rect")
-	          .attr("x", -candleWidth / 2)
-	          .attr("width", candleWidth)
-	          .attr("y", function(d) { return yScale(Math.max(d.open, d.close)); })
-	          .attr("height", function(d) { return Math.abs(yScale(d.open) - yScale(d.close))+.01; });
-	      d3.transition(candle.exit())
-	          .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; })
-	          .style("opacity", 1e-6).remove();
+	    var candleUpdate = d3.transition(candle.classed("up", function(d) { return d.open <= d.close; }))
+	        .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; });
+	    candleUpdate.select("line")
+	        .attr("y1", function(d) { return yScale(d.low); })
+	        .attr("y2", function(d) { return yScale(d.high); });
+	    candleUpdate.select("rect")
+	        .attr("x", -candleWidth / 2)
+	        .attr("width", candleWidth)
+	        .attr("y", function(d) { return yScale(Math.max(d.open, d.close)); })
+	        .attr("height", function(d) { return Math.abs(yScale(d.open) - yScale(d.close))+.5; });
+	    candleUpdate.select(".high")
+	    	.attr("transform", function(d) { return "translate(0," + yScale(d.high) + ")"; });
+	    candleUpdate.select(".low")
+	    	.attr("transform", function(d) { return "translate(0," + yScale(d.low) + ")"; });	
+	    d3.transition(candle.exit())
+	        .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; })
+	        .style("opacity", 1e-6).remove();
 	
-	      //line.attr("d", d3.svg.line().x(function(d) { return xScale(d.time); }).y(function(d) { return volumeScale(d.volume); }));
+	    //line.attr("d", d3.svg.line().x(function(d) { return xScale(d.time); }).y(function(d) { return volumeScale(d.volume); }));
 	
-	      // Update the x-axis.
-	      g.select(".x.axis")
-	          .attr("transform", "translate(0," + yScale.range()[0] + ")")
-	          .call(xAxis);
+	    // Update the x-axis.
+	    g.select(".x.axis")
+	        .attr("transform", "translate(0," + yScale.range()[0] + ")")
+	        .call(xAxis);
 	
-	      // Update the left axis.
-	      g.select(".left.axis")
-	          .call(leftAxis);
+	    // Update the left axis.
+	    g.select(".left.axis")
+	        .call(leftAxis);
 	
-	      // Update the y-axis.
-	      g.select(".y.axis")
-	      	  .attr("transform", "translate(" + xScale.range()[1] + ", 0)")
-	          .call(yAxis);
+	    // Update the y-axis.
+	    g.select(".y.axis")
+	    	.attr("transform", "translate(" + xScale.range()[1] + ", 0)")
+	        .call(yAxis);
 	
-	  		svg.transition().duration(300).style("opacity", 1);
-			loader.transition().duration(300).style("opacity", 0);
+	    hover.style("opacity",1);
+		horizontal.style("opacity",1);
+	  	focus.style("opacity",1);
+	  	
+	  	svg.transition().duration(300).style("opacity", 1);
+		loader.transition().duration(300).style("opacity", 0);
 					
-	      function mousemove() {
+	    function mousemove() {
 	        var tx = Math.max(options.margin.left, Math.min(options.width+options.margin.left, d3.mouse(this)[0])),
 	        	x = d3.bisect(lineData.map(function(d) { return d.time; }), xScale.invert(tx-options.margin.left));
 	            d = lineData[x];
+	  	    
 	        if (d) {
 	        	var details = div.select('.chartDetails');
 		        details.html("<span class='date'>"+ parseDate(d, increment) + 
-						"</span><span>O:<b>" + d.open.toFixed(4)  + "</b></span>" +
-						"<span class='high'>H:<b>" + d.high.toFixed(4) + "</b></span>" +
-						"<span class='low'>L:<b>" + d.low.toFixed(4) + "</b></span>" +
-						"<span>C:<b>" + d.close.toFixed(4)  + "</b></span>" +
-						"<span class='volume'>Volume:<b>" + d.volume.toFixed(4) + " " + self.trade.currency + "</b></span>")
-			  			.style("opacity",1);
+					"</span><span>O:<b>" + d.open.toFixed(4)  + "</b></span>" +
+					"<span class='high'>H:<b>" + d.high.toFixed(4) + "</b></span>" +
+					"<span class='low'>L:<b>" + d.low.toFixed(4) + "</b></span>" +
+					"<span>C:<b>" + d.close.toFixed(4)  + "</b></span>" +
+					"<span class='volume'>Volume:<b>" + d.volume.toFixed(4) + " " + self.trade.currency + "</b></span>")
+			  		.style("opacity",1);
+			  		
+				hover.attr("transform", "translate(" + xScale(d.time) + ")");
+				focus.attr("transform", "translate(" + xScale(d.time) + "," + yScale(d.close) + ")");
+				horizontal.attr("x1", xScale(d.time));
+		  		horizontal.attr("x2", options.width);
+		  		horizontal.attr("y1", yScale(d.close));
+		  		horizontal.attr("y2", yScale(d.close));
 			}
-		  			    
-	        hover.attr("transform", "translate(" + xScale(d.time) + ")");
-	     }	    	
+	    }	    	
 	}
 }	
 
