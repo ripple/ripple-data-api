5f77fcdd7905a67d1e95f1773bdd5e315fd2d64f
Matthew Fettig <matthew@ripple.com>
2013-12-13 16:53:13 -0800

    volume bars on line chart

--- trade/rippleCharts.js
+++ trade/rippleCharts.js
@@ -1,15 +1,28 @@
 var RippleCharts = {};
 
 RippleCharts.line = function (options){
-	
+
 	this.options  = options;
 	var lineData = [];
 	var self     = this;
-	  		
+		
+  	var //x     = d3.time.scale().range([0, options.width]).domain(d3.extent(self.lineData, function(d) { return d.time; })),
+    	//y     = d3.scale.linear().range([options.height, 0]).domain(d3.extent(self.lineData, function(d) { return d.close; })).nice(),
+      	xScale      = d3.time.scale();
+      	priceScale  = d3.scale.linear();
+      	volumeScale = d3.scale.linear();
+      	xAxis       = d3.svg.axis().scale(xScale),
+      	priceAxis   = d3.svg.axis().scale(priceScale).orient("right"),
+      	volumeAxis  = d3.svg.axis().scale(volumeScale).orient("left").tickFormat(d3.format("s")),
+      	line  = d3.svg.line()
+        	.x(function(d) { return xScale(d.time); })
+        	.y(function(d) { return priceScale(d.close); });	
+	      		
 	if (options.title)   d3.select("body").append("h3").html(options.title);
-	if (!options.margin) options.margin = {top: 10, right: 50, bottom: 50, left: 0};
+	if (!options.margin) options.margin = {top: 10, right: 50, bottom: 50, left: 50};
 	if (!options.height) options.height = 500;
-	if (!options.width)  options.width  = 1000;
+	if (!options.width)  options.width  = 950;
+	
 	
 	var div = d3.select(options.id);
 	
@@ -29,6 +42,8 @@ RippleCharts.line = function (options){
 	  	.attr("y", options.margin.top)
 	  	.attr("height", options.height)
 	  	.attr("width", options.width); 
+	  	
+	  	
 	  		  	
 	 //var status = svg.append("text").attr("class", "status").attr({y:"1em", x:options.margin.left ,fill:"#999"});
 	var status  = div.append("h4")
@@ -45,11 +60,41 @@ RippleCharts.line = function (options){
 		.attr("class", "hover")
 		.style("opacity", 0);  	
 		
+	var focus = svg.append("circle")
+ 		.attr("class", "focus")
+ 		.attr("r", 4.5)
+ 		.style("opacity", 0);
+ 				
 	var loader = div.append("img")
 		.attr("class", "loader")
 		.attr("src", "images/throbber5.gif")
 		.style("opacity", 0);	
 
+  	var gEnter = svgEnter.append("g")
+      	.attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
+
+	
+  	gEnter.append("g")
+  		.attr("class", "x axis");
+  	gEnter.append("g")
+  		.attr("class", "volume axis")
+  		.append("text").text("Volume")
+  		.attr("class", "title")
+  		.attr("transform", "rotate(-90)")
+  		.attr("y",15).attr("x",-90);
+  	gEnter.append("g")
+  		.attr("class", "price axis")
+  		.append("text").text("Price")
+  		.attr("class", "title")
+  		.attr("transform", "rotate(-90)")
+  		.attr("y",-10).attr("x",-80);
+  		
+   	gEnter.append("g").attr("class", "volumeBars")
+  		.attr("width", options.width + options.margin.left + options.margin.right)
+	  	.attr("height", options.height + options.margin.top + options.margin.bottom);  		
+	  	
+  	gEnter.append("path").attr("class", "line");
+  		
 	this.fadeOut = function () {
 		svg.transition().duration(100).style("opacity", 0);
 		svg.on("mousemove.hover", "");
@@ -111,46 +156,71 @@ RippleCharts.line = function (options){
 			status.html("No Data for this Period").style("opacity",1);
 			return;	
 		}
-		
+
+		barWidth = options.width/(lineData.length*1.15);
+		if (barWidth<2) barWidth = 2; 
+				
 		self.lineData = lineData;
+	    
+	    var g      = svg.select("g"), 
+	    	status = div.select(".status"),     	
+	   		line   = d3.svg.line()
+       			.x(function(d) { return xScale(d.time); })
+           		.y(function(d) { return priceScale(d.close); });
+           	
+	  	g.select(".volumeBars").selectAll("rect").data(lineData).enter().append("rect"); 
+	  	g.select(".axis.price").select("text").text("Price ("+self.trade.currency+")");
+		g.select(".axis.volume").select("text").text("Volume ("+self.base.currency+")");
 		
-	  	var x     = d3.time.scale().range([0, options.width]).domain(d3.extent(self.lineData, function(d) { return d.time; })),
-	    	y     = d3.scale.linear().range([options.height, 0]).domain(d3.extent(self.lineData, function(d) { return d.close; })).nice(),
-	      	xAxis = d3.svg.axis().scale(x),
-	      	yAxis = d3.svg.axis().scale(y),
-	      	line  = d3.svg.line()
-	        	.x(function(d) { return x(d.time); })
-	        	.y(function(d) { return y(d.close); });
-	    	        	
-	  	var gEnter = svgEnter.append("g")
-	      	.attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
-	      	
-	  	gEnter.append("path").attr("class", "line");
-	  	gEnter.append("g")
-	      	.attr("class", "x axis")
-	      	.attr("transform", "translate(0," + y.range()[0] + ")")
-	  	gEnter.append("g")
-	    	.attr("class", "y axis right")
-	      	.attr("transform", "translate(" + x.range()[1] + ")");
-	  	var g = svg.select("g");
-	  	
-	  	g.select(".line").datum(self.lineData).attr("d", line);
-	  	g.select(".x.axis").call(xAxis);
-	  	g.select(".y.axis.right").call(yAxis.orient("right"));
-	  	
-	  	svg.select(".focus").remove();
-	   	var focus = svg.append("g")
-	 		.attr("class", "focus")
-	 		.style("opacity", 0);	
-	      
-	    focus.append("circle").attr("r", 4.5);	
+	    // Update the x-scale.
+	    xScale
+	        .domain(RippleCharts.getExtents(lineData))
+	        .range([0, options.width]);
 	
+	    // Update the volume scale.
+	    volumeScale
+	        .domain([0, d3.max(lineData, function (d) {return d.volume})*2])
+	        .range([options.height, 0]);
+	
+	    // Update the y-scale.
+	    priceScale
+	    	.domain([
+	        	d3.min(lineData, function(d) { return d.close; })*.975,
+	            d3.max(lineData, function(d) { return d.close; })*1.025
+	        ])
+	  		.range([options.height, 0]);
+	  		
+		// Update the x-axis.
+        g.select(".x.axis")
+            .attr("transform", "translate(0," + priceScale.range()[0] + ")")
+            .call(xAxis);
+
+        // Update the price axis.
+        g.select(".price.axis")
+                .attr("transform", "translate(" + xScale.range()[1] + ", 0)")
+            .call(priceAxis);	
+    
+        // Update the volume axis.
+        g.select(".volume.axis")
+            .call(volumeAxis);
+
+	  	g.select(".line").datum(self.lineData).attr("d", line);
+
+		//add the volume bars
+		g.select('.volumeBars').selectAll("rect").data(lineData)
+			.attr("x", function(d){return xScale(d.time)-barWidth/3})
+			.attr("y", function(d){return volumeScale(d.volume)})
+			.attr("width", barWidth/1.5)
+			.attr("height", function(d){return options.height - volumeScale(d.volume)})
+			.exit().remove();
+			
+				
 	  	svg.transition().duration(300).style("opacity", 1);
 		loader.transition().duration(300).style("opacity", 0);
 		
 		function mousemove() {
 			var tx = Math.max(options.margin.left, Math.min(options.width+options.margin.left, d3.mouse(this)[0])),
-				i  = d3.bisect(self.lineData.map(function(d) { return d.time; }), x.invert(tx-options.margin.left));
+				i  = d3.bisect(self.lineData.map(function(d) { return d.time; }), xScale.invert(tx-options.margin.left));
 		    	d  = self.lineData[i-1];
 		    
 		    //console.log(i);
@@ -172,15 +242,15 @@ RippleCharts.line = function (options){
 		    }
 		    
 		 	if (d) {
-		 		tx = x(d.time)+options.margin.left;
-		 		ty = y(d.close)+options.margin.top;
+		 		tx = xScale(d.time)+options.margin.left;
+		 		ty = priceScale(d.close)+options.margin.top;
 				
 		  		details.html("<span class='date'>"+ parseDate(d, increment) + 
 					"</span><span>O:<b>" + d.open.toFixed(4)  + "</b></span>" +
 					"<span class='high'>H:<b>" + d.high.toFixed(4) + "</b></span>" +
 					"<span class='low'>L:<b>" + d.low.toFixed(4) + "</b></span>" +
 					"<span>C:<b>" + d.close.toFixed(4)  + "</b></span>" +
-					"<span class='volume'>Volume:<b>" + d.volume.toFixed(4) + " " + self.trade.currency + "</b></span>")
+					"<span class='volume'>Volume:<b>" + d.volume.toFixed(4) + " " + self.base.currency + "</b></span>")
 		  			.style("opacity",1);
 		  		
 		  		hover.transition().duration(50).attr("transform", "translate(" + tx + ")");
@@ -199,13 +269,12 @@ RippleCharts.line = function (options){
 
 RippleCharts.candlestick = function (options) {
 	var self = this,
-    	candleWidth = 10,
       	xScale      = d3.time.scale.utc(),
       	priceScale  = d3.scale.linear(),
       	volumeScale = d3.scale.linear(),
       	xAxis       = d3.svg.axis().scale(xScale),
-      	leftAxis    = d3.svg.axis().scale(volumeScale).orient("left").tickFormat(d3.format("s")),
-      	rightAxis   = d3.svg.axis().scale(priceScale).orient("right");
+      	volumeAxis    = d3.svg.axis().scale(volumeScale).orient("left").tickFormat(d3.format("s")),
+      	priceAxis   = d3.svg.axis().scale(priceScale).orient("right");
 
 	if (!options.margin) options.margin = {top: 10, right: 50, bottom: 50, left: 50};
 	if (!options.height) options.height = 500;
@@ -233,28 +302,23 @@ RippleCharts.candlestick = function (options) {
   	var gEnter = svg.append("g")
   		.attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
   	gEnter.append("rect").attr("class", "background").attr("width", options.width).attr("height", options.height);
-  	gEnter.append("g").attr("class", "volume")
-  		.attr("width", options.width + options.margin.left + options.margin.right)
-	  	.attr("height", options.height + options.margin.top + options.margin.bottom);   
+  	gEnter.append("g").attr("class", "volumeBars").attr("clip-path", "url(#clip)");   
   	gEnter.append("g").attr("class", "candlesticks").attr("clip-path", "url(#clip)");
   	gEnter.append("g").attr("class", "x axis");
-  	gEnter.append("g").attr("class", "left axis")
+  		
+  	gEnter.append("g").attr("class", "volume axis")   
   		.append("text").text("Volume")
   		.attr("class", "title")
   		.attr("transform", "rotate(-90)")
-  		.attr("y",15).attr("x",-60);
-  	gEnter.append("g")
-  		.attr("class", "y axis")
+  		.attr("y",15).attr("x",-90);
+  		
+  	gEnter.append("g").attr("class", "price axis")
   		.append("text").text("Price")
   		.attr("class", "title")
   		.attr("transform", "rotate(-90)")
   		.attr("y",-10).attr("x",-80);
-    gEnter.append("circle").attr("class", "focus").attr("r",3).attr("fill", "#555");
-    
-    var hover      = gEnter.append("line").attr("class", "hover").attr("y2", options.height);
-  	var horizontal = gEnter.append("line").attr("class", "hover");
-  	      
-	// gradient for volume bars	    
+  	
+  	// gradient for volume bars	    
     var gradient = svg.append("svg:defs")
 		.append("svg:linearGradient")
 		.attr("id", "gradient")
@@ -273,6 +337,10 @@ RippleCharts.candlestick = function (options) {
 	    .attr("offset", "100%")
 	    .attr("stop-color", "#eee")
 	    .attr("stop-opacity", 1);	
+	    	
+    var hover      = gEnter.append("line").attr("class", "hover").attr("y2", options.height);
+  	var horizontal = gEnter.append("line").attr("class", "hover");
+  	var focus      = gEnter.append("circle").attr("class", "focus dark").attr("r",3).attr("fill", "#555");
 	          
 	this.fadeOut = function () {
 		div.selectAll("svg").transition().duration(100).style("opacity", 0);
@@ -338,13 +406,10 @@ RippleCharts.candlestick = function (options) {
 	
 	    var g      = svg.select("g");
 		var status = div.select(".status");
-		var focus  = g.select(".focus");
-		
-		var line = g.select(".volume").selectAll(".line").data(function(d) { return [d]; });
-	    line.enter().append("path").attr("class", "line");
-		
-		g.select(".volume").selectAll("rect").data(lineData).enter().append("rect");  
-		g.select(".axis.y").select("text").text("Price ("+self.trade.currency+")");
+		 
+		g.select(".axis.price").select("text").text("Price ("+self.trade.currency+")");
+		g.select(".axis.volume").select("text").text("Volume ("+self.base.currency+")");
+		g.select(".volumeBars").selectAll("rect").data(lineData).enter().append("rect"); 
 		
 	    // add the candlesticks.
 	    var candle = g.select(".candlesticks").selectAll("g").data(function(d) { return d; }, function(d) { return d.time; });
@@ -353,19 +418,19 @@ RippleCharts.candlestick = function (options) {
 	    candleEnter.append("line")
 	        .attr("y1", function(d) { return priceScale(.5 * (d.low + d.high)); })
 	        .attr("y2", function(d) { return priceScale(.5 * (d.low + d.high)); });
+	    candleEnter.append("line")
+	    	.attr("class", "high");
+	    candleEnter.append("line")
+	    	.attr("class", "low")    
 	    candleEnter.append("rect")
 	        .attr("x", -candleWidth / 2)
 	        .attr("y", function(d) { return priceScale(.5 * (d.open + d.close)); })
 	        .attr("height", function(d) { return Math.abs(priceScale(d.open) - priceScale(d.close))+.5; })
-	        .attr("width", candleWidth);
-	    candleEnter.append("line")
-	    	.attr("class", "high");
-	    candleEnter.append("line")
-	    	.attr("class", "low")	
+	        .attr("width", candleWidth);	
 
 	    // Update the x-scale.
 	    xScale
-	        .domain(getExtents(lineData))
+	        .domain(RippleCharts.getExtents(lineData))
 	        .range([0, options.width]);
 	
 	    // Update the volume scale.
@@ -381,9 +446,11 @@ RippleCharts.candlestick = function (options) {
 	            d3.max(lineData, function(d) { return Math.max(d.open, d.close, d.high, d.low); })*1.025
 	        ])
 	  		.range([options.height, 0]);
+	  		
+	  		
 
 		//add the volume bars
-		g.select('.volume').selectAll("rect").data(lineData)
+		g.select('.volumeBars').selectAll("rect").data(lineData)
 			.attr("x", function(d){return xScale(d.time)-candleWidth/3})
 			.attr("y", function(d){return volumeScale(d.volume)})
 			.attr("width", candleWidth/1.5)
@@ -413,21 +480,15 @@ RippleCharts.candlestick = function (options) {
 	        .attr("transform", function(d) { return "translate(" + xScale(d.time) + ")"; })
 	        .style("opacity", 1e-6).remove();
 	
-	    //line.attr("d", d3.svg.line().x(function(d) { return xScale(d.time); }).y(function(d) { return volumeScale(d.volume); }));
-	
 	    // Update the x-axis.
-	    g.select(".x.axis")
-	        .attr("transform", "translate(0," + priceScale.range()[0] + ")")
-	        .call(xAxis);
-	
-	    // Update the left axis.
-	    g.select(".left.axis")
-	        .call(leftAxis);
-	
+	    g.select(".x.axis").call(xAxis).attr("transform", "translate(0," + priceScale.range()[0] + ")")
+
 	    // Update the y-axis.
-	    g.select(".y.axis")
-	    	.attr("transform", "translate(" + xScale.range()[1] + ", 0)")
-	        .call(rightAxis);
+	    g.select(".price.axis").call(priceAxis).attr("transform", "translate(" + xScale.range()[1] + ", 0)")
+	    	
+	    // Update the left axis.
+	    g.select(".volume.axis").call(volumeAxis);
+	
 	  	
 	  	svg.transition().duration(300).style("opacity", 1);
 		loader.transition().duration(300).style("opacity", 0);
@@ -462,14 +523,14 @@ RippleCharts.candlestick = function (options) {
 			}
 	    }	    	
 	}
-	
-	function getExtents (lineData) {
+}	
+
+RippleCharts.getExtents = function (lineData) {
 		if (lineData.length>1) {
-			var difference = (lineData[1].time - lineData[0].time)/2000;
-			console.log(difference);
-			
+			var difference = (lineData[1].time - lineData[0].time)/1000;
+
 			return [
-				d3.time.second.offset(d3.min(lineData, function(d) { return d.time }), -difference),
+				d3.min(lineData, function(d) { return d.time }),
 				d3.time.second.offset(d3.max(lineData, function(d) { return d.time }), difference)
 			]
 			
@@ -477,8 +538,6 @@ RippleCharts.candlestick = function (options) {
 		
 		return d3.extent(lineData, function(d) { return d.volume; });	
 	}
-}	
-
 
 var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ];
