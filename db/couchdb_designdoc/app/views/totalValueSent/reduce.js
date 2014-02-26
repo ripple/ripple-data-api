4be8de4db19c4a9e6e505b0a8d9d4b45416cd53b
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-02-26 15:00:15 -0800

    [FEATURE] New totalValueSent view

--- db/couchdb_designdoc/app/views/totalValueSent/reduce.js
+++ db/couchdb_designdoc/app/views/totalValueSent/reduce.js
@@ -1,22 +1,10 @@
 function(keys, values) {
   var sum = 0;
   values.forEach(function(val, index){
-    var to_add;
-    if (typeof val === 'number') {
-      to_add = val;
-    } else if (typeof val === 'string') {
-      to_add = parseFloat(val);
-
-      if (typeof to_add !== 'number') {
-        log('bad value: ' + val);
-        return;
-      }
-    } else {
-      log('bad value: ' + val);
-      return;
+    var to_add = parseFloat(val);
+    if (typeof to_add === 'number') {
+      sum += to_add;
     }
-
-    sum += to_add;
   });
   return sum;
 }
