3831760c84713270ba311ef5c5f5cf0259b66b5a
Matthew Fettig <matthew@ripple.com>
2014-03-04 17:53:37 -0800

    couchdb_designdoc no longer used

--- db/couchdb_designdoc/app/lists/streamRows.js
+++
@@ -1,6 +0,0 @@
-function(head, req) {
-    var row;
-    while(row = getRow()){
-        send(JSON.stringify([row.key, row.value]) + "\n");
-    }
-}
--- db/couchdb_designdoc/app/views/ledgerClosesByTime/map.js
+++
@@ -1,8 +0,0 @@
-function (doc) {
-  var time = new Date( doc.close_time_timestamp ),
-    timestamp = [ time.getUTCFullYear( ), time.getUTCMonth( ), time.getUTCDate( ),
-      time.getUTCHours( ), time.getUTCMinutes( ), time.getUTCSeconds( )
-    ];
-
-  emit( timestamp, 1 );
-}
