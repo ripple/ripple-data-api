6d18a77d59529d814ad061618ab5fde72ae8b543
Jaka Hudoklin <jakahudoklin@gmail.com>
2015-03-23 01:26:32 +0100

    Create updateViews, integrate with purgeViews

--- db/updateViews.js
+++ db/updateViews.js
@@ -19,29 +19,44 @@ db.list({ startkey:'_design/', endkey:'_e' }, function(err, res){
 
   var designDocIds = _.map(res.rows, function(row){ return row.key; });
 
-  // get design docs
-  db.fetch({keys: designDocIds}, function(err, res){
-    if (err) return winston.error('problem getting design docs: ' + err);
-    var docs = res.rows;
+  //compile new docs
+  compile(__dirname + "/design", function(err, design) {
+    if (err) return winston.error('problem compiling design docs: ' + err);
 
-    if (!docs.length) return winston.info('no exising design docs found');
+    // get design docs
+    db.fetch({keys: designDocIds}, function(err, res){
+      if (err) return winston.error('problem getting design docs: ' + err);
+      var docs = res.rows;
 
-    //compile new docs
-    compile(__dirname + "/design", function(err, design) {
-      if (err) return winston.error('problem compiling design docs: ' + err);
-      var ids = {}, purge = [];
+      if (!docs.length) winston.info('no exising design docs found');
+
+      var ids = {}, idsu = {}, purge = [], insert = [];
       for (var name in design) ids[design[name]._id] = name;
+      for (var name in docs) idsu[docs[name].id] = name;
 
       docs.forEach(function(doc){
         if (!ids[doc.id]) purge.push({id:doc.id, rev:doc.doc._rev});
       });
 
-      if (!purge.length) return winston.info("no design documents to remove");
+      for (var name in design) {
+        if (!idsu[design[name]._id]) insert.push(design[name]);
+      }
+
+      if (!insert.length) winston.info("no design documents to create");
+
+      insert.forEach(function(doc) {
+        db.insert(doc, doc._id, function(err, res) {
+          if (err) return winston.error("problem inserting doc: ", doc._id, err);
+          winston.info(doc._id + " succesfully inserted");
+        });
+      });
+
+      if (!purge.length) winston.info("no design documents to remove");
 
       purge.forEach(function(doc) {
         db.destroy(doc.id, doc.rev, function(err, res){
           if (err) return winston.error('problem removing doc: ', doc.id, err);
-          winston.info(doc.id+" sucessfully removed");
+          winston.info(doc.id + " sucessfully removed");
         });
       });
     });
