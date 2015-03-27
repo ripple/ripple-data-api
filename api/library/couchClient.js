f484a880e5cf462c0f8638c1f04c43d34450ebe5
Matthew Fettig <matthew@ripple.com>
2015-03-27 15:36:30 -0700

    reformat logs for sumologic parsing

--- api/library/couchClient.js
+++ api/library/couchClient.js
@@ -1,28 +1,33 @@
 var env     = process.env.NODE_ENV || "development";
 var winston = require('winston');
+var moment  = require('moment');
 
 function init (params) {
   var client = require('nano')(params);
-  
+
   client.parentView = client.view;
   client.view       = function(doc, view, options, callback) {
-    var label = "", d = Date.now()
+    var d     = Date.now();
+    var label = "";
     var tags; //tracking elapsed time
     var suffix;
-    
+    var rowcount;
+
     if (options.label) {
       label         = options.label;
-      options.label = undefined;  
-    } 
-    
+      options.label = undefined;
+    }
+
     tags = [doc+'.'+view];
     if (label) tags.push(label.replace(':','_'));
-    
+
     suffix = tags.join('.');
     statsd.increment('couchDB.request.' + suffix, null);
-    
+
     client.parentView(doc, view, options, function(error, response){
-      if (error) { 
+      var date  = "["+(moment.utc().format())+"]";
+
+      if (error) {
         if (error.code === 'EMFILE' || error.code === 'EADDRINFO' || error.code === 'ENOTFOUND') {
           error = 'Too Many Connections';
         } else if (error.code === 'ECONNRESET') {
@@ -31,15 +36,26 @@ function init (params) {
           error = 'Request Timeout';
         }
       }
-      
-      d = Date.now()-d;
-      if (DEBUG) winston.info('CouchDB - '+doc+'/'+view, label, (d/1000)+'s');
-      
-      statsd.timing('couchDB.responseTime.' + suffix, d);    
+
+      //return response
       callback(error, response);
+
+      if (response && response.rows) rowcount = response.rows.length;
+
+      //log stats
+      d = (Date.now()-d)/1000;
+
+      winston.info(date,
+        'COUCHDB',
+        'view:' + suffix,
+        'time:' + d + 's',
+        rowcount ? 'rowcount:' + rowcount : '',
+        error ? ('ERROR:' + error) : '');
+
+      statsd.timing('couchDB.responseTime.' + suffix, d);
     });
   }
-  
+
   return client;
 }
 
